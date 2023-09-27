eabc73f6936fa73304ad8daa864e0f6efec6946b
Rjae <rjae.easton@gmail.com>
2023-09-27 16:24:44 -0400

    Create repository

---
+++ Domain/Ordr.cs
@@ -0,0 +1,35 @@
+﻿namespace Domain;
+
+public class Ordr
+{
+    public required Address BillingAddress { get; set; }
+
+    public required string Contents { get; set; }
+
+    public Customer Customer { get; set; } = null!;
+
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public required Address ShippingAddress { get; set; }
+}
+
+public record Address
+{
+    public required string City { get; init; }
+    public required string Country { get; init; }
+    public required string Line1 { get; init; }
+    public string? Line2 { get; init; }
+    public required int PostalCode { get; init; }
+    public required string State { get; init; }
+}
+
+public class Customer
+{
+    public required Address Address { get; set; }
+
+  public Guid Id { get; set; } = Guid.NewGuid();
+
+    public required string Name { get; set; }
+
+    public List<Ordr> Orders { get; } = new List<Ordr>();
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# What is this for?
+
+TBD
+
+# Who is this for?
+
+TBD
+
+# What is here?
+
+## Database
+
+TBD
+
+## Domain
+
+TBD
+
+## Tests
+
+TBD
+
+## Docker Compose
+
+TBD
+
+# What are the dependencies?
+
+- Docker
+
+# How do I run?
+
+```shell
+~/projects/csharp-ide-showdown # make test-all
+```
