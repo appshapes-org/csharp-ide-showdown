bb9d6633809c3c414eaa7cdf99dc5a09e5de97f7
Rjae <rjae.easton@gmail.com>
2023-09-29 09:59:24 -0400

    Move type to file

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -12,29 +12,3 @@ public class Order
 
     public required Address ShippingAddress { get; set; }
 }
-
-public record Address
-{
-    public required string City { get; init; }
-
-    public required string Country { get; init; }
-
-    public required string Line1 { get; init; }
-
-    public string? Line2 { get; init; }
-
-    public required int PostalCode { get; init; }
-
-    public required string State { get; init; }
-}
-
-public class Customer
-{
-    public required Address Address { get; set; }
-
-    public Guid Id { get; set; } = Guid.NewGuid();
-
-    public required string Name { get; set; }
-
-    public List<Order> Orders { get; } = new List<Order>();
-}
