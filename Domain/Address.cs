6830f2b0ea6abc367b2a94e85c7d4f1ef5176d55
Rjae <rjae.easton@gmail.com>
2023-09-29 09:59:15 -0400

    Move type to file

---
+++ Domain/Address.cs
@@ -0,0 +1,11 @@
+namespace Domain;
+
+public record Address
+{
+    public required string City { get; init; }
+    public required string Country { get; init; }
+    public required string Line1 { get; init; }
+    public string? Line2 { get; init; }
+    public required int PostalCode { get; init; }
+    public required string State { get; init; }
+}
