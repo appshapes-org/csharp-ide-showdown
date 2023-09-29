99d70207bb1b91f2c9d750f92f7b9b6f0faa577f
Rjae <rjae.easton@gmail.com>
2023-09-29 11:36:18 -0400

    Unit testing

--- Domain/Address.cs
+++ Domain/Address.cs
@@ -6,6 +6,6 @@ public record Address
     public required string Country { get; init; }
     public required string Line1 { get; init; }
     public string? Line2 { get; init; }
-    public required int PostalCode { get; init; }
+    public required string PostalCode { get; init; }
     public required string State { get; init; }
 }
